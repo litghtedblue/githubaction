8a991ac018835a1ebed9cf047058fa048fca5fe4
lightedblue <lightedblue@gmail.com>
2021-01-30 19:29:12 +0900

    add modle

---
+++ parent/demo/src/test/java/com/example/demo/WebdemoApplicationTests.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@SpringBootTest
+class WebdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	public void exampleTest2() {
+		assertThat(3,is(3));
+	}
+
+}
