4636cf6724af3970f09699607515a2ecbe4f51a5
lightedblue <lightedblue@gmail.com>
2021-02-01 00:03:22 +0900

    ch

--- parent/demo/src/main/java/com/example/demo/WebdemoApplication.java
+++ parent/demo/src/main/java/com/example/demo/WebdemoApplication.java
@@ -7,11 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebdemoApplication {
 
 	public static void main(String[] args) {
+		Public p = Public.builder().abc("abcdef").abc2(2222).build();
+		System.out.println(p);
+
 		SpringApplication.run(WebdemoApplication.class, args);
 	}
 
 	public void abc(){
-		new Public();
 		new Public("");
+		Public p = Public.builder().abc("aaa").build();
 	}
 }
--- parent/demo/src/test/java/com/example/demo/WebdemoApplicationITest.java
+++ parent/demo/src/test/java/com/example/demo/WebdemoApplicationITest.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.apache.commons.lang3.math.NumberUtils;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,16 +9,25 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 @SpringBootTest
-class WebdemoApplicationTests {
+class WebdemoApplicationITest {
 
 	@Test
+	@Tag("fast")
 	void contextLoads() {
 	}
 
 	@Test
+	@Tag("fast")
 	public void exampleTest2() {
 		assertThat(true,is(NumberUtils.isDigits("1234567")));
 		assertThat(5,is(5));
+	}
+
+	@Test
+	@Tag("large")
+	public void exampleTest3() {
+		assertThat(true,is(NumberUtils.isDigits("1234567")));
+		assertThat(4,is(4));
 	}
 
 }
